09e73117188d28a1940b0f9aac2c1b012d5611fd
ramirune <timpiga3@hotmail.com>
2022-05-10 17:19:06 +0200

    confetti

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+import { startConfetti, stopConfetti, removeConfetti } from './confetti.js';
+
 const playerScoreEl = document.getElementById('playerScore');
-let playerChoiceEl = document.getElementById('playerChoice');
+const playerChoiceEl = document.getElementById('playerChoice');
 const computerScoreEl = document.getElementById('computerScore');
-let computerChoiceEl = document.getElementById('computerChoice');
+const computerChoiceEl = document.getElementById('computerChoice');
 const resultText = document.getElementById('resultText');
 
 const playerRock = document.getElementById('playerRock');
@@ -35,6 +37,8 @@ function resetSelected() {
 	allGameIcons.forEach(icon => {
 		icon.classList.remove('selected');
 	});
+	stopConfetti();
+	removeConfetti();
 }
 
 // Reset Score & playerChoice/computerChoice
@@ -48,6 +52,7 @@ function resetAll() {
 	resultText.textContent = '';
 	resetSelected();
 }
+window.resetAll = resetAll;
 
 // Random Computer Choice
 function computerRandomChoice() {
@@ -101,6 +106,7 @@ function updateScore(playerChoice) {
 	} else {
 		const choice = choices[playerChoice];
 		if (choice.defeats.indexOf(computerChoice) > -1) {
+			startConfetti();
 			resultText.textContent = 'You Won!';
 			playerScoreNumber++;
 			playerScoreEl.textContent = playerScoreNumber;
@@ -149,6 +155,7 @@ function select(playerChoice) {
 			break;
 	}
 }
+window.select = select;
 
 // On startup, set initial value
 resetAll();
